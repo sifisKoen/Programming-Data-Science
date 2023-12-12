e5b8432ef6717b131c4206fe319f7fe391de9be4
Gard Aasness <gardaasness@gmail.com>
2023-12-12 16:16:21 +0100

    Create methods for split, transform and started on training/evaluating

---
+++ Assignment4/model.py
@@ -0,0 +1,75 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+# Models to test and their corresponding AUC scores
+models_and_auc_scores = {"Random Forest": [RandomForestClassifier(), 0], "Naive Bayes": [GaussianNB(), 0], "Logistic Regression": [LogisticRegression(), 0], "SVM": [SVC(), 0], "KNN": [KNeighborsClassifier(), 0], "Gradient Boosting": [GradientBoostingClassifier(), 0], "Decision Tree": [DecisionTreeClassifier(), 0]}
+
+# Divide the data into training and validation sets, where 'ACTIVE' is the target variable
+def split(df):
+    X = df.drop('ACTIVE', axis=1)
+    y = df['ACTIVE']
+    # validation set size is 20% of the total data, training set size is 80%
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_valid, y_train, y_valid
+
+# Feature preprocessing pipeline (transfom data)
+def transform_pipeline(Xy_train):
+    X_train, _ = Xy_train
+
+    # Identify numerical and categorical columns
+    numeric_features = X_train.select_dtypes(include=['float64']).columns
+    categorical_features = X_train.select_dtypes(include=['object']).columns
+
+    # Create a pipeline for numerical columns which imputes missing values with the median and scales the data
+    numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())
+        ])
+
+    # Create a pipeline for categorical columns which imputes missing values with 'missing'
+    categorical_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+        ])
+
+    # Create a preprocessor which applies the numerical and categorical pipelines to the correct columns
+    preprocessor = ColumnTransformer(transformers=[
+            ('num', numeric_transformer, numeric_features),
+            ('cat', categorical_transformer, categorical_features)
+        ])
+
+    return preprocessor
+
+def train_models(Xy_train, preprocessor):
+    X_train, y_train = Xy_train
+    # Loop through each model in models_and_auc_scores and train it
+    for model_name, (model, auc_score) in models_and_auc_scores.items():
+        model_pipeline = Pipeline(steps=[
+            ('preprocessor', preprocessor), 
+            (model_name, model)
+        ])
+        model_pipeline.fit(preprocessor.fit_transform(X_train), y_train)
+
+def evaluate_models(Xy_train, Xy_valid):
+    X_train, y_train = Xy_train
+    X_valid, y_valid = Xy_valid
+
+    # iterate through each model in models_and_auc_scores
+    for model in models_and_auc_scores:
+        # fit the model
+        models_and_auc_scores[model][0].fit(X_train, y_train)
+        # calculate the AUC score
+        models_and_auc_scores[model][1] = roc_auc_score(y_valid, models_and_auc_scores[model][0].predict_proba(X_valid)[:,1])
+
+    # sort the models_and_auc_scores dictionary by the AUC score
+    sorted_models_and_auc_scores = sorted(models_and_auc_scores.items(), key=lambda x: x[1][1], reverse=True)
+    # print the sorted dictionary
+    print(sorted_models_and_auc_scores)
