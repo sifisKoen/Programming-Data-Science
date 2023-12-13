7fdebf2d196a70d47559e6f8d107471ba7044aa9
Gard Aasness <gardaasness@gmail.com>
2023-12-13 09:20:45 +0100

    Finish training and evaluate to find the best baseline model

--- Assignment4/model.py
+++ Assignment4/model.py
@@ -1,6 +1,9 @@
+from sklearn import svm
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import StandardScaler
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
@@ -8,37 +11,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
+from sklearn.preprocessing import OrdinalEncoder
 from sklearn.tree import DecisionTreeClassifier
+from app import *
 
 # Models to test and their corresponding AUC scores
-models_and_auc_scores = {"Random Forest": [RandomForestClassifier(), 0], "Naive Bayes": [GaussianNB(), 0], "Logistic Regression": [LogisticRegression(), 0], "SVM": [SVC(), 0], "KNN": [KNeighborsClassifier(), 0], "Gradient Boosting": [GradientBoostingClassifier(), 0], "Decision Tree": [DecisionTreeClassifier(), 0]}
+models_and_auc_scores = {"Random Forest": [RandomForestClassifier(), 0], "Naive Bayes": [GaussianNB(), 0], "Logistic Regression": [LogisticRegression(), 0], "KNN": [KNeighborsClassifier(), 0], "Gradient Boosting": [GradientBoostingClassifier(), 0], "Decision Tree": [DecisionTreeClassifier(), 0], "Extreme Random Forest": [ExtraTreesClassifier(), 0]}
 
 # Divide the data into training and validation sets, where 'ACTIVE' is the target variable
 def split(df):
+    print("Splitting data...")
     X = df.drop('ACTIVE', axis=1)
     y = df['ACTIVE']
     # validation set size is 20% of the total data, training set size is 80%
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.5, random_state=42)
     return X_train, X_valid, y_train, y_valid
 
 # Feature preprocessing pipeline (transfom data)
-def transform_pipeline(Xy_train):
-    X_train, _ = Xy_train
+def transform_pipeline(X_train):
+    print("Transforming data...")
 
     # Identify numerical and categorical columns
     numeric_features = X_train.select_dtypes(include=['float64']).columns
-    categorical_features = X_train.select_dtypes(include=['object']).columns
+    categorical_features = X_train.select_dtypes(include=['object', 'bool']).columns
 
-    # Create a pipeline for numerical columns which imputes missing values with the median and scales the data
+    # Create a pipeline for numerical columns which imputes missing values with the median and applies PCA
     numeric_transformer = Pipeline(steps=[
             ('imputer', SimpleImputer(strategy='median')),
-            ('scaler', StandardScaler())
+            ('pca', PCA(n_components=0.95)),
         ])
 
-    # Create a pipeline for categorical columns which imputes missing values with 'missing'
+    # Create a pipeline for categorical columns which imputes missing values with 'missing' and encodes the values
     categorical_transformer = Pipeline(steps=[
             ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+            ('encoder', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))
         ])
 
     # Create a preprocessor which applies the numerical and categorical pipelines to the correct columns
@@ -46,23 +52,31 @@ def transform_pipeline(Xy_train):
             ('num', numeric_transformer, numeric_features),
             ('cat', categorical_transformer, categorical_features)
         ])
-
+    
     return preprocessor
 
-def train_and_evaluate_models(Xy_train, Xy_valid, preprocessor):
-    X_train, y_train = Xy_train
-    X_valid, y_valid = Xy_valid
+def train_and_evaluate_models(X_train, X_valid, y_train, y_valid, preprocessor):
+    print("Training and evaluating models...")
+
     # Loop through each model in models_and_auc_scores and train it
-    for model_name, (model, auc_score) in models_and_auc_scores.items():
+    for model_name, (model, _) in models_and_auc_scores.items():
+        print(f"Making pipeline for {model_name}...")
         model_pipeline = Pipeline(steps=[
-            ('preprocessor', preprocessor), 
+            ('preprocessor', preprocessor),
+            ('scaler', StandardScaler()),
             (model_name, model)
         ])
         # Fit each model
+        print(f"Fitting model for {model_name}...")
         model_pipeline.fit(X_train, y_train)
 
         # Make predictions on the validation set
-        y_pred_prob = model.predict_proba(X_valid)[:, 1]
+        try:
+            print(f"Predicting for {model_name}...")
+            y_pred_prob = model_pipeline.predict_proba(X_valid)[:, 1]
+        except ValueError as e:
+            print(f"Error for model {model_name}: {e}")
+            continue
 
         # Calculate AUC score for the model
         auc = roc_auc_score(y_valid, y_pred_prob)
@@ -70,34 +84,48 @@ def train_and_evaluate_models(Xy_train, Xy_valid, preprocessor):
         # Update the AUC score for the model in the dictionary
         models_and_auc_scores[model_name][1] = auc
 
-        print ("Model:", model_name, "AUC score:", auc)
+        print ("Model:", model_name, "- AUC score:", auc)
     print("Best model:", max(models_and_auc_scores, key=lambda x: models_and_auc_scores[x][1]))
-    
-def evaluate_models(Xy_train, Xy_valid):
+    print("AUC scores:", models_and_auc_scores)
+
+def tune_best_model(Xy_train, preprocessor):
+    print("Tuning best model...")
     X_train, y_train = Xy_train
-    X_valid, y_valid = Xy_valid
+    best_model = max(models_and_auc_scores, key=lambda x: models_and_auc_scores[x][1])
+    return None
 
-    # iterate through each model in models_and_auc_scores
-    for model in models_and_auc_scores:
-        # fit the model
-        models_and_auc_scores[model][0].fit(X_train, y_train)
-        # calculate the AUC score
-        models_and_auc_scores[model][1] = roc_auc_score(y_valid, models_and_auc_scores[model][0].predict_proba(X_valid)[:,1])
+def test_best_model(Xy_test, preprocessor):
+    print("Testing best model...")
+    X_test, y_test = Xy_test
 
-    # sort the models_and_auc_scores dictionary by the AUC score
-    sorted_models_and_auc_scores = sorted(models_and_auc_scores.items(), key=lambda x: x[1][1], reverse=True)
-    # print the sorted dictionary
-    print(sorted_models_and_auc_scores)
+    return None
+
+def predict_activity(model, Xy_test):
+    print("Predicting activity...")
+    return None
 
 def main():
+    df = load_data("training_smiles.csv")
+    mole = extract_mole(df)
+    #all_features = extract_features(df)
+    final_features = extract_top_features(df)
+    clean_df = clean_data(final_features)
+
+    # Train and evaluate models
+    X_train, X_valid, y_train, y_valid = split(clean_df)
+    preprocessor = transform_pipeline(X_train)
+    train_and_evaluate_models(X_train, X_valid, y_train, y_valid, preprocessor)
+
+    #DONE: 
     # Split data
     # Transform data
     # Train models
     # Evaluate models
-    # Predict with best model on test set
 
-    #TODO: fix imbalance dataset
-    return 0
+    #TODO: 
+    # fix imbalanced dataset
+    # Tune model hyperparameters
+    # Predict with best model on test set
 
 if __name__ == "__main__":
     main()
